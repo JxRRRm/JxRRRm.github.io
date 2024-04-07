47376d8f1cbd8b2da4258cb8c7524ad92048dc7e
Jeremy Vuong <jeremyvuong1995@gmail.com>
2024-04-06 19:19:28 -0700

    Assignment 5 - Fun with JavaScript
    Upload 2 files:
    fancifymytext.html and fancifymytext.js

---
+++ fancifymytext.js
@@ -0,0 +1,38 @@
+function showAlert() {
+  alert("Hello, world!");
+}
+
+function makeTextBigger() {
+  document.getElementById("textArea").style.fontSize = "24pt";
+}
+
+function applyFancyStyles() {
+  var textArea = document.getElementById("textArea");
+  // selecting the "FancyShmancy" radio button, should make the text bold, underline, and blue.
+  if (document.getElementById("fancy").checked) {
+      textArea.style.fontWeight = "bold";
+      textArea.style.color = "blue";
+      textArea.style.textDecoration = "underline";
+  } else {
+      textArea.style.fontWeight = "normal"; // Reset font weight to normal
+      textArea.style.color = "black"; // Reset color to default
+      textArea.style.textDecoration = "none"; // Reset text decoration to default
+  }
+}
+
+function transformText() {
+  var textArea = document.getElementById("textArea");
+  var text = textArea.value.toUpperCase(); // Uppercase the entire text
+  var parts = text.split(".");  // Separate sentences
+  text = parts.join("-Moo."); // Add a suffix of "-Moo" to the last word of each sentence
+  // Update the text area with the modified text
+  textArea.value = text;
+}
+
+function clear() {
+  // Clear radio buttons
+  var radios = document.getElementsByName('fanciness');
+  radios.forEach(radio => radio.checked = false);
+  // Clear the text area
+  document.getElementById("textArea").value = ""; 
+}
